b6b02e1145fa8fad13f123dc6ac0e674a9edab3e
Gunzee00 <gsinaga53@gmail.com>
2024-09-23 10:38:09 +0700

    push

--- controllers/authcontroller/authcontroller.go
+++ controllers/authcontroller/authcontroller.go
@@ -3,62 +3,121 @@ package authcontroller
 import (
 	"encoding/json"
 	"net/http"
+	"time"
+
+	"github.com/jeypc/go-jwt-mux/config"
+
+	"github.com/golang-jwt/jwt/v4"
+	"github.com/jeypc/go-jwt-mux/helper"
+	"gorm.io/gorm"
 
 	"github.com/jeypc/go-jwt-mux/models"
 	"golang.org/x/crypto/bcrypt"
 )
 
-// Handler for Login
 func Login(w http.ResponseWriter, r *http.Request) {
-	// Logic for login
-}
 
-// Handler for Register
-func Register(w http.ResponseWriter, r *http.Request) {
+	// mengambil inputan json
 	var userInput models.User
-
-	// Decode request body
 	decoder := json.NewDecoder(r.Body)
 	if err := decoder.Decode(&userInput); err != nil {
-		http.Error(w, "Gagal memproses JSON input", http.StatusBadRequest)
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJSON(w, http.StatusBadRequest, response)
 		return
 	}
 	defer r.Body.Close()
 
-	// Validasi input
-	if userInput.Username == "" || userInput.NamaLengkap == "" || userInput.Password == "" {
-		http.Error(w, "Data tidak lengkap", http.StatusBadRequest)
+	// ambil data user berdasarkan username
+	var user models.User
+	if err := models.DB.Where("username = ?", userInput.Username).First(&user).Error; err != nil {
+		switch err {
+		case gorm.ErrRecordNotFound:
+			response := map[string]string{"message": "Username atau password salah"}
+			helper.ResponseJSON(w, http.StatusUnauthorized, response)
+			return
+		default:
+			response := map[string]string{"message": err.Error()}
+			helper.ResponseJSON(w, http.StatusInternalServerError, response)
+			return
+		}
+	}
+
+	// cek apakah password valid
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(userInput.Password)); err != nil {
+		response := map[string]string{"message": "Username atau password salah"}
+		helper.ResponseJSON(w, http.StatusUnauthorized, response)
 		return
 	}
 
-	// Hash password
-	hashPassword, err := bcrypt.GenerateFromPassword([]byte(userInput.Password), bcrypt.DefaultCost)
+	// proses pembuatan token jwt
+	expTime := time.Now().Add(time.Minute * 1)
+	claims := &config.JWTClaim{
+		Username: user.Username,
+		RegisteredClaims: jwt.RegisteredClaims{
+			Issuer:    "go-jwt-mux",
+			ExpiresAt: jwt.NewNumericDate(expTime),
+		},
+	}
+
+	// medeklarasikan algoritma yang akan digunakan untuk signing
+	tokenAlgo := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// signed token
+	token, err := tokenAlgo.SignedString(config.JWT_KEY)
 	if err != nil {
-		http.Error(w, "Gagal mengenkripsi password", http.StatusInternalServerError)
-		return
-	}
-	userInput.Password = string(hashPassword)
-
-	// Insert ke database
-	if err := models.DB.Create(&userInput).Error; err != nil {
-		http.Error(w, "Gagal menyimpan data user", http.StatusInternalServerError)
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJSON(w, http.StatusInternalServerError, response)
 		return
 	}
 
-	// Kirim response sukses
-	response, err := json.Marshal(map[string]string{"message": "User registered successfully"})
-	if err != nil {
-		http.Error(w, "Gagal membuat response", http.StatusInternalServerError)
-		return
-	}
+	// set token yang ke cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Path:     "/",
+		Value:    token,
+		HttpOnly: true,
+	})
 
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(response)
+	response := map[string]string{"message": "login berhasil"}
+	helper.ResponseJSON(w, http.StatusOK, response)
 }
 
-// Handler for Logout
+func Register(w http.ResponseWriter, r *http.Request) {
+
+	// mengambil inputan json
+	var userInput models.User
+	decoder := json.NewDecoder(r.Body)
+	if err := decoder.Decode(&userInput); err != nil {
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJSON(w, http.StatusBadRequest, response)
+		return
+	}
+	defer r.Body.Close()
+
+	// hash pass menggunakan bcrypt
+	hashPassword, _ := bcrypt.GenerateFromPassword([]byte(userInput.Password), bcrypt.DefaultCost)
+	userInput.Password = string(hashPassword)
+
+	// insert ke database
+	if err := models.DB.Create(&userInput).Error; err != nil {
+		response := map[string]string{"message": err.Error()}
+		helper.ResponseJSON(w, http.StatusInternalServerError, response)
+		return
+	}
+
+	response := map[string]string{"message": "success"}
+	helper.ResponseJSON(w, http.StatusOK, response)
+}
+
 func Logout(w http.ResponseWriter, r *http.Request) {
-	// Logic for logout
-	w.Write([]byte("Logout successful"))
+	// hapus token yang ada di cookie
+	http.SetCookie(w, &http.Cookie{
+		Name:     "token",
+		Path:     "/",
+		Value:    "",
+		HttpOnly: true,
+		MaxAge:   -1,
+	})
+
+	response := map[string]string{"message": "logout berhasil"}
+	helper.ResponseJSON(w, http.StatusOK, response)
 }
